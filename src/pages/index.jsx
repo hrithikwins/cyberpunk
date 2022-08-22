2a3b1e2f266970c6e3a3368d3a9cc1e5fd6c4c29
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2022-08-22 20:07:55 +0530

    man in scene

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
 import * as THREE from "three"
-import React, { Suspense } from "react"
+import React, { Suspense, useState, useRef } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
-import { Model } from "../components/Scene"
 import { OrbitControls } from "@react-three/drei"
+import { ManInSuit } from "../components/man_in_suit"
+import { PubG } from "../components/Pubg"
+import { ParkScene } from "../components/ParkScene"
 // function Rig() {
 //   return useFrame((state) => {
 //     state.camera.position.x = THREE.MathUtils.lerp(state.camera.position.x, 1 + state.mouse.x / 4, 0.075)
@@ -11,25 +13,54 @@ import { OrbitControls } from "@react-three/drei"
 // }
 
 export default function Index() {
+  const [manPosition, setManPosition] = useState([0, 0, 0]);
+
   return (
-    <Canvas concurrent shadowMap
+    <Canvas
+      // concurrent={true} shadowMap={true}
       // camera={{ position: [2, 4.5, 2.5], fov: 502 }}
       style={{ height: "100vh", width: "100vw" }}>
-      <ambientLight intensity={2} />
-      <OrbitControls />
-      <directionalLight position={[-5, 5, 5]} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} />
-      <directionalLight position={[-15, 15, 15]} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} />
-      <directionalLight position={[-25, 25, 25]} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} />
-      <group position={[0, -1, 0]}>
-        <Suspense fallback={null}>
-          <Model />
-        </Suspense>
-      </group>
+      {/* <ambientLight /> */}
+      {/* <directionalLight color="red" position={[0, 120, 20]} castShadow={true} /> */}
+      <directionalLight position={[0, 100, 100]} color={0xffffff} intensity={0.8} castShadow={true} />
+      <directionalLight position={[-100, 100, 100]} color={0xffffff} intensity={0.8} castShadow={true} />
+      <directionalLight position={[100, -100, 100]} color={0xffffff} intensity={0.8} castShadow={true} />
+      <directionalLight position={[100, 100, -100]} color={0xffffff} intensity={0.8} castShadow={true} />
+      <directionalLight position={[-100, -100, 100]} color={0xffffff} intensity={0.8} castShadow={true} />
+      <directionalLight position={[0, -100, -100]} color={0xffffff} intensity={0.8} castShadow={true} />
+      <directionalLight position={[-100, 100, -100]} color={0xffffff} intensity={0.8} castShadow={true} />
+      <directionalLight position={[-100, -100, -100]} color={0xffffff} intensity={0.8} castShadow={true} />
+      <InnerComponents />
+
       <mesh rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -1, 0]} receiveShadow>
         <planeBufferGeometry args={[10, 10, 1, 1]} />
         <shadowMaterial transparent opacity={0.2} />
       </mesh>
+      <OrbitControls />
       {/* <Rig /> */}
     </Canvas>
   )
+}
+
+
+const InnerComponents = () => {
+
+  // const controls = useRef();
+  const [xPos, setXPos] = useState(0);
+  // useFrame((state) => controls.current.update());
+  useFrame((state) => setXPos(() => xPos > 1000 ? 0 : xPos + 1));
+  return (
+    <group position={[0, -5, 0]}>
+      <Suspense fallback={null}>
+        <ManInSuit
+          position={[xPos, 15, 10]} angle={0.3}
+
+        />
+        {/* <PubG /> */}
+        <ParkScene />
+        {/* <Model /> */}
+      </Suspense>
+
+    </group>
+  );
 }
---
+++ three.bash
@@ -0,0 +1,2 @@
+# npx gltfjsx ./public/models/park-facility/scene.gltf
+npx gltfjsx ./public/models/man-in-suit/scene.gltf
