d901d5c57155d54d0307b5ed24e293d5a5d9a50c
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2022-08-24 13:47:28 +0530

    optimization

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,14 @@
 import * as THREE from "three"
-import React, { Suspense, useState, useCallback, useRef, useEffect } from "react"
-import { Canvas, useFrame } from "@react-three/fiber"
+import React, { Suspense, useState, useMemo, useCallback, useRef, useEffect } from "react"
+import { Canvas, useFrame, useThree, } from "@react-three/fiber"
 import { Html, OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { ManInSuit } from "../components/man_in_suit"
 import { PubG } from "../components/Pubg"
 import { ParkScene } from "../components/ParkScene"
 import { Physics, useBox, usePlane } from "@react-three/cannon"
+import CameraControls from 'camera-controls'
+import { useControls } from 'leva'
+
 // function Rig() {
 //   return useFrame((state) => {
 //     state.camera.position.x = THREE.MathUtils.lerp(state.camera.position.x, 1 + state.mouse.x / 4, 0.075)
@@ -30,19 +33,45 @@ export default function Index() {
       document.removeEventListener('keydown', handleKeyPress);
     };
   }, [handleKeyPress]);
+  const { model } = useControls({
+    model: {
+      value: 'Day', options: [
+        "Day", "Night"
+      ]
+    }
+  })
+
+  function Controls({ zoom, focus, pos = new THREE.Vector3(), look = new THREE.Vector3() }) {
+    const camera = useThree((state) => state.camera)
+    const gl = useThree((state) => state.gl)
+    const controls = useMemo(() => new CameraControls(camera, gl.domElement), [])
+    return useFrame((state, delta) => {
+      zoom ? pos.set(focus.x, focus.y, focus.z + 0.2) : pos.set(0, 0, 5)
+      zoom ? look.set(focus.x, focus.y, focus.z - 0.2) : look.set(0, 0, 4)
+
+      state.camera.position.lerp(pos, 0.5)
+      state.camera.updateProjectionMatrix()
+
+      controls.setLookAt(state.camera.position.x, state.camera.position.y, state.camera.position.z, look.x, look.y, look.z, true)
+      return controls.update(delta)
+    })
+  }
 
   return (
     <>
       <span>{manPosition}</span>
       <Canvas
-        // camera={{ position: [-30, 35, -15], }}
+        // camera={{ position: [manPosition[0] + 10, manPosition[1], manPosition[2]], }}
+
+        camera={{ position: [0, 5, -10], }}
         // concurrent={true} shadowMap={true}
 
         style={{ height: "100vh", width: "100%" }}>
-        <PerspectiveCamera
+        {/* <PerspectiveCamera
           args={[75, window.innerWidth / window.innerHeight, 0.1, 1000]}
           near={20}
-        />
+          lookAt={[0, 0, 0]}
+        /> */}
 
         {/* <ambientLight /> */}
         {/* <directionalLight color="red" position={[0, 120, 20]} castShadow={true} /> */}
@@ -82,21 +111,48 @@ export default function Index() {
           <InnerComponents position={manPosition} setPosition={setManPosition} />
           <Plane rotation={[-Math.PI / 2, 0, 0]}
             position={[0, 0, 0]} />
-          <group scale={[0.02, 0.02, 0.02]} position={[0, -1, 0]}>
+          {/* <group scale={[0.02, 0.02, 0.02]} position={[0, -1, 0]}>
             <ParkScene />
 
-          </group>
+          </group> */}
           {/* <mesh rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -1, 0]} receiveShadow >
           <planeBufferGeometry args={[10, 10, 1, 1]} />
           <shadowMaterial transparent opacity={0.2} />
         </mesh>
         <Rig /> */}
+
         </Physics>
+        {/* <Controls zoom={[0, 0, 0]} focus={manPosition} /> */}
         <OrbitControls enableZoom={true} minPolarAngle={Math.PI / 4} />
 
       </Canvas>
     </>
   )
+}
+
+function Controls({ zoom, focus, pos = new THREE.Vector3(), look = new THREE.Vector3() }) {
+  const camera = useThree((state) => state.camera)
+  const gl = useThree((state) => state.gl)
+  const controls = useMemo(() => new CameraControls(camera, gl.domElement), [])
+  return useFrame((state, delta) => {
+    zoom ? pos.set(focus.x, focus.y, focus.z + 0.2) : pos.set(0, 0, 5)
+    zoom ? look.set(focus.x, focus.y, focus.z - 0.2) : look.set(0, 0, 4)
+
+    state.camera.position.lerp(pos, 0.5)
+    state.camera.updateProjectionMatrix()
+
+    controls.setLookAt(state.camera.position.x, state.camera.position.y, state.camera.position.z, look.x, look.y, look.z, true)
+    return controls.update(delta)
+  })
+}
+
+function Cloud({ momentsData, zoomToView }) {
+  return momentsData.map(({ position, color }, i) => (
+    <mesh key={i} position={position} onClick={(e) => zoomToView(e.object.position)}>
+      <boxGeometry args={[0.1, 0.08, 0.003]} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  ))
 }
 
 
